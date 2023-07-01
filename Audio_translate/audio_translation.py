7899f3f42bae0041ba6db42aa3b03c1cf5bb51cd
Sweta Swain <sweta.swain@stud.fra-uas.de>
2023-07-01 12:24:20 +0200

    translated api code

--- Audio_translate/audio_translation.py
+++ Audio_translate/audio_translation.py
@@ -4,11 +4,10 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 audio_file = open("german_song.mp4", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-#translate = openai.Audio.translate("whisper-1", audio_file)
+translate = openai.Audio.translate("whisper-1", audio_file)
 
 
-print(transcript["text"])#,translate["text"])
+print(translate["text"])
 #or write the converted text into a text document
-# with open("translated.txt", "w") as f:
-#     f.write(translate["text"])
+with open("translated.txt", "w") as f:
+    f.write(translate["text"])
